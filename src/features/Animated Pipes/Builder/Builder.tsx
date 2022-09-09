daf72ad39622a9c346ad6fb7a96e3eab50846703
Marek Fodor <fodormarek5@gmail.com>
2022-09-09 13:01:53 +0200

    Added nodes appending

--- src/features/Animated Pipes/Builder/Builder.tsx
+++ src/features/Animated Pipes/Builder/Builder.tsx
@@ -1,24 +1,23 @@
 import { Box } from "@chakra-ui/react";
 import { MutableRefObject, ReactNode, RefObject } from "react";
-import Frame, { BorderOptions, FrameOptions } from "../Components/Frame";
+import Frame, { FrameOptions } from "../Components/Frame";
 import NodeObject from "../Components/Node";
+import Pipe from "../Components/Pipe";
+import { BorderOptions, NodeType } from "../Components/Types";
 
 class Builder {
+  private static mainNode: Frame;
+  private nodes: Array<NodeType>;
   borderDefaultOptions;
   frameDefaultOptions;
-  nodes: NodeObject[];
   anchorPoint: number;
 
-  constructor(
-    anchorPoint: number,
-    borderOptions?: BorderOptions,
-    frameOptions?: FrameOptions
-  ) {
-    this.nodes = new Array<NodeObject>();
+  constructor(anchorPoint: number, frameOptions?: FrameOptions) {
+    this.nodes = new Array<NodeType>();
     this.anchorPoint = anchorPoint;
 
-    if (borderOptions) {
-      this.borderDefaultOptions = borderOptions;
+    if (frameOptions?.borderOptions) {
+      this.borderDefaultOptions = frameOptions?.borderOptions;
     } else {
       this.borderDefaultOptions = new BorderOptions(4, "solid", "purple");
     }
@@ -38,15 +37,106 @@ class Builder {
   }
 
   //TODO spravit aby mohol byt anchor aj v lavo alebo v pravo
-  getMainFrame(): JSX.Element {
-    const frame = new Frame(this.frameDefaultOptions, {
-      top: this.anchorPoint,
-    });
-    this.nodes.push(frame);
-    return frame.getFrame();
+  getMainFrame(): Frame {
+    if (!Builder.mainNode) {
+      Builder.mainNode = new Frame("main-node", this.frameDefaultOptions, {
+        top: this.anchorPoint,
+      });
+    }
+    this.nodes.push(Builder.mainNode);
+    return Builder.mainNode;
   }
 
-  appendLeft(existingObject: NodeObject, objectToAppend: NodeObject) {}
+  appendLeft(existingObject: NodeType, objectToAppend: NodeType) {
+    let overlap = 0;
+    if (existingObject instanceof Frame && objectToAppend instanceof Pipe) {
+      overlap =
+        existingObject.getFrameOptions().getBorderOptions().getThickness() + 1;
+    } else if (
+      existingObject instanceof Pipe &&
+      objectToAppend instanceof Frame
+    ) {
+      overlap =
+        objectToAppend.getFrameOptions().getBorderOptions().getThickness() + 1;
+    }
+    let existingObjectXStart = existingObject.getXStart() + overlap;
+    let existingObjectYCenter = existingObject.getYCenter();
+
+    objectToAppend.setXEnd(existingObjectXStart);
+    objectToAppend.setYCenter(existingObjectYCenter);
+    this.nodes.push(objectToAppend);
+  }
+
+  appendRight(existingObject: NodeType, objectToAppend: NodeType) {
+    let overlap = 0;
+    if (existingObject instanceof Frame && objectToAppend instanceof Pipe) {
+      overlap = -(
+        existingObject.getFrameOptions().getBorderOptions().getThickness() + 1
+      );
+    } else if (
+      existingObject instanceof Pipe &&
+      objectToAppend instanceof Frame
+    ) {
+      overlap = -(
+        objectToAppend.getFrameOptions().getBorderOptions().getThickness() + 1
+      );
+    }
+
+    let existingObjectXEnd = existingObject.getXEnd() + overlap;
+    let existingObjectYCenter = existingObject.getYCenter();
+
+    objectToAppend.setXStart(existingObjectXEnd);
+    objectToAppend.setYCenter(existingObjectYCenter);
+    this.nodes.push(objectToAppend);
+  }
+
+  appendBottom(existingObject: NodeType, objectToAppend: NodeType) {
+    let overlap = 0;
+    if (existingObject instanceof Frame && objectToAppend instanceof Pipe) {
+      overlap = -(
+        existingObject.getFrameOptions().getBorderOptions().getThickness() + 1
+      );
+    } else if (
+      existingObject instanceof Pipe &&
+      objectToAppend instanceof Frame
+    ) {
+      overlap = -(
+        objectToAppend.getFrameOptions().getBorderOptions().getThickness() + 1
+      );
+    }
+
+    let existingObjectXCenter = existingObject.getXCenter();
+    let existingObjectYEnd = existingObject.getYEnd() + overlap;
+
+    objectToAppend.setXCenter(existingObjectXCenter);
+    objectToAppend.setYStart(existingObjectYEnd);
+    this.nodes.push(objectToAppend);
+  }
+
+  appendUp(existingObject: NodeType, objectToAppend: NodeType) {
+    let overlap = 0;
+    if (existingObject instanceof Frame && objectToAppend instanceof Pipe) {
+      overlap =
+        existingObject.getFrameOptions().getBorderOptions().getThickness() + 1;
+    } else if (
+      existingObject instanceof Pipe &&
+      objectToAppend instanceof Frame
+    ) {
+      overlap =
+        objectToAppend.getFrameOptions().getBorderOptions().getThickness() + 1;
+    }
+
+    let existingObjectXCenter = existingObject.getXCenter();
+    let existingObjectYStart = existingObject.getYStart() + overlap;
+
+    objectToAppend.setXCenter(existingObjectXCenter);
+    objectToAppend.setYEnd(existingObjectYStart);
+    this.nodes.push(objectToAppend);
+  }
+
+  getNodes(): NodeType[] {
+    return this.nodes;
+  }
 }
 
 export default Builder;
