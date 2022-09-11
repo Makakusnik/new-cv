2846147c1d2517e00ada7fa38eafa169011d9f98
Marek Fodor <fodormarek5@gmail.com>
2022-09-11 12:02:27 +0200

    Added Joint and frame creation factory funcs

--- src/features/Animated Pipes/Builder/NodeFactory.tsx
+++ src/features/Animated Pipes/Builder/NodeFactory.tsx
@@ -1,5 +1,5 @@
-import { FrameOptionsType } from "../Components/Frame";
-import { JointOptionsType } from "../Components/Joint";
+import Frame, { FrameOptionsType, FrameSize } from "../Components/Frame";
+import Joint, { JointOptionsType, JointRotation } from "../Components/Joint";
 import Pipe, { PipeOptionsType } from "../Components/Pipe";
 import { BorderOptionsType } from "../Components/Types";
 
@@ -8,9 +8,9 @@ class NodeObjectFactory {
   private defaultJointOptions: JointOptionsType;
   private defaultFrameOptions: FrameOptionsType;
   private defaultPipeOptions: PipeOptionsType;
-  private static numOfPipes: number;
-  private static numOfJoints: number;
-  private static numOfFrames: number;
+  private static numOfPipes: number = 0;
+  private static numOfJoints: number = 0;
+  private static numOfFrames: number = 0;
 
   constructor(
     defaultBorderOptions: BorderOptionsType,
@@ -25,18 +25,56 @@ class NodeObjectFactory {
   }
 
   createHorizontalPipe(length: number): Pipe {
-    return new Pipe(`pipe${NodeObjectFactory.numOfPipes}`, {
+    NodeObjectFactory.numOfPipes++;
+    return new Pipe(`pipe${NodeObjectFactory.numOfPipes++}`, {
       ...this.defaultPipeOptions,
+      borderOptions: this.defaultBorderOptions,
       orientation: "horizontal",
       length: length,
     });
   }
 
   createVerticalPipe(length: number): Pipe {
-    return new Pipe(`pipe${NodeObjectFactory.numOfPipes}`, {
+    NodeObjectFactory.numOfPipes++;
+    return new Pipe(`pipe${NodeObjectFactory.numOfPipes++}`, {
       ...this.defaultPipeOptions,
+      borderOptions: this.defaultBorderOptions,
       orientation: "vertical",
       length: length,
+    });
+  }
+
+  createKneeJoint(rotation?: JointRotation): Joint {
+    return new Joint(`joint${NodeObjectFactory.numOfJoints++}`, {
+      ...this.defaultJointOptions,
+      borderOptions: this.defaultBorderOptions,
+      type: "knee",
+      rotation: rotation,
+    });
+  }
+
+  createTJoint(rotation?: JointRotation): Joint {
+    return new Joint(`joint${NodeObjectFactory.numOfJoints++}`, {
+      ...this.defaultJointOptions,
+      borderOptions: this.defaultBorderOptions,
+      type: "T",
+      rotation: rotation,
+    });
+  }
+
+  createCrossJoint(): Joint {
+    return new Joint(`joint${NodeObjectFactory.numOfJoints++}`, {
+      ...this.defaultJointOptions,
+      borderOptions: this.defaultBorderOptions,
+      type: "cross",
+    });
+  }
+
+  createFrame(size?: FrameSize): Frame {
+    return new Frame(`frame${NodeObjectFactory.numOfFrames++}`, {
+      ...this.defaultFrameOptions,
+      borderOptions: this.defaultBorderOptions,
+      size: size,
     });
   }
 }
