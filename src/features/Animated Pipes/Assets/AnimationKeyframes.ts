30ac4f005638134e9b011696b566676c11b95584
Marek Fodor <fodormarek5@gmail.com>
2022-09-15 17:41:44 +0200

    Few tweaks to keyframes

--- src/features/Animated Pipes/Assets/AnimationKeyframes.ts
+++ src/features/Animated Pipes/Assets/AnimationKeyframes.ts
@@ -3,17 +3,35 @@ import { keyframes } from "@emotion/react";
 export const basicProjectileMovement = keyframes`
     0% {
       offset-distance: 0%;
+      opacity: 0;
+      transform: rotate(0);
+    } 
+    10%{
+      offset-distance: calc(0% + 20px);
       opacity: 0
     }
-    30% {
+    15% {
+      transform: rotate(360deg);
       opacity: 1
     }
-    80% {
+    25% {
+      opacity: 1;
+      transform: rotate(720deg);
+    }
+    95% {
+      offset-distance: calc(100% - 50px);
+      transform: rotate(2000deg);
+      opacity: 1;
+    }
+    98% {
+      offset-distance: calc(100% - 35px);
       transform:scale(1);
+      transform: rotate(2200deg);
       opacity: 1;
     }
     100% {
       transform:scale(2);
+      transform: rotate(2300deg);
       offset-distance: 100%;
       opacity: 0;
     }
