55bb997b9479e7132447c1e1d9108ea6e73f014e
Marek Fodor <fodormarek5@gmail.com>
2022-10-07 17:04:18 +0200

    Removed state from SkillComponent

--- src/components/UI/SkillShowcase/SkillComponent.tsx
+++ src/components/UI/SkillShowcase/SkillComponent.tsx
@@ -1,26 +1,39 @@
 import { Box, HStack, Text, useToken, VStack } from "@chakra-ui/react";
-import { ReactNode } from "react";
+import styled from "@emotion/styled";
+import { ReactNode, useState } from "react";
+import { IconWrapper } from "../Icon";
 
 interface SkillContainerProps {
   title: string;
   children: ReactNode;
   experience: 1 | 2 | 3 | 4 | 5;
+  color: string;
 }
 
-export const SkillCompononent = ({ title, children, experience }: SkillContainerProps) => {
+export const SkillCompononent = ({ title, children, experience, color }: SkillContainerProps) => {
   let [secondary400, secondary700] = useToken("colors", ["secondary.400", "secondary.700"]);
-
+  const [hover, setHover] = useState(false);
   return (
-    <VStack minWidth="80px">
+    <VStack
+      minWidth="80px"
+      onMouseOver={() => {
+        setHover(true);
+      }}
+      onMouseLeave={() => {
+        setHover(false);
+      }}
+    >
       <Text>{title}</Text>
-      {children}
+      <IconWrapper activate={hover} color={color}>
+        {children}
+      </IconWrapper>
       <HStack spacing="4px">
-        {[1, 2, 3, 4, 5].map((item) => {
+        {[1, 2, 3, 4, 5].map((item, index) => {
           let color = secondary700;
           if (item > experience) {
             color = secondary400;
           }
-          return <Box borderRadius="100%" width="6px" height="6px" backgroundColor={color} />;
+          return <Box key={index} borderRadius="100%" width="6px" height="6px" backgroundColor={color} />;
         })}
       </HStack>
     </VStack>
