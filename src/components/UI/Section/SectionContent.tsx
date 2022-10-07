29c8a47d6ddb4b3b30f7ef768f192cde8770cdff
Marek Fodor <fodormarek5@gmail.com>
2022-10-07 17:05:11 +0200

    Removed  children wrapper of content component

--- src/components/UI/Section/SectionContent.tsx
+++ src/components/UI/Section/SectionContent.tsx
@@ -3,12 +3,13 @@ import { ReactNode } from "react";
 
 interface SectionContentProps {
   children: ReactNode | ReactNode[];
+  direction?: "column" | "row";
 }
 
-export const SectionContent = ({ children }: SectionContentProps) => {
+export const SectionContent = ({ children, direction = "column" }: SectionContentProps) => {
   return (
-    <Container m={0} minWidth="full" padding="0 16px" lineHeight="1.8rem">
-      <Text>{children}</Text>
+    <Container display="flex" m={0} minWidth="full" columnGap={"32px"} flexDirection={direction} padding="0 16px" lineHeight="1.8rem">
+      {children}
     </Container>
   );
 };
