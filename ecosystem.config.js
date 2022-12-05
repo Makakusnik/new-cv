bff6e8aa405c3856283f7dcc7bcb7fa79e096f74
Marek Fodor <fodormarek5@gmail.com>
2022-12-05 09:27:23 +0000

    Update from prod

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name   : "Unfinished CV",
+    script : "npm run start",
+	log_date_format: "YYYY-MM-DD HH:mm Z",
+	env_production: {
+	NODE_ENV: "production"
+}
+  }]
+}
